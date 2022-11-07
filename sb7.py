f7f8d4d586deb8558e123c67b36259a99b482ed8
Timothy Geirk <71552349+tjgeirk@users.noreply.github.com>
2022-11-06 16:02:34 -0800

    Update sb7.py

--- sb7.py
+++ sb7.py
@@ -1,15 +1,14 @@
+import time
 import datetime
 from ccxt import kucoinfutures as kcf
 from pandas import DataFrame as dataframe
 from ta import momentum, trend, volatility
 
-STOP = 0.05
-TRIGGER = 0.03
-INIT_STOP = 0.1
 LEVERAGE = 20
-LOTS = 50
 TIMEFRAMES = ['1m']
-COINS = ["RNDR"]
+COINS = ["OP"]
+DCA_INTERVAL_DELAY_TIME: 0
+DCA_LOTS_PER_INTERVAL = 1
 
 exchange = kcf({
     'apiKey': '',
@@ -48,8 +47,7 @@ def getPositions():
     df = {}
     for coin in COINS:
         df[coin] = {}
-        for _, col in enumerate(['contracts', 'side', 'percentage'
-                                 ]):
+        for _, col in enumerate(['contracts', 'side', 'percentage', 'unrealizedPnl']):
             df[coin][col] = 0
             for (_, v) in enumerate(positions):
                 if v['symbol'] == coin:
@@ -62,86 +60,86 @@ def buy():
     if getPositions()[coin]['side'] != 'short':
         try:
             return exchange.create_limit_order(
-                coin, 'buy', LOTS,
+                coin, 'buy', DCA_LOTS_PER_INTERVAL,
                 exchange.fetch_order_book(coin)['bids'][0][0],
                 {'leverage': LEVERAGE})
-        except Exception as e:
-            print(e)
+        except Exception:
+            pass
     elif getPositions()[coin]['side'] == 'short':
         try:
             return exchange.create_limit_order(
                 coin, 'buy', getPositions()[coin]['contracts'],
                 exchange.fetch_order_book(coin)['bids'][0][0],
                 {'closeOrder': True, 'reduceOnly': True})
-        except Exception as e:
-            print(e)
+        except Exception:
+            pass
 
 
 def sell():
     if getPositions()[coin]['side'] != 'long':
         try:
             return exchange.create_limit_order(
-                coin, 'sell', LOTS,
+                coin, 'sell', DCA_LOTS_PER_INTERVAL,
                 exchange.fetch_order_book(coin)['asks'][0][0],
                 {'leverage': LEVERAGE})
-        except Exception as e:
-            print(e)
+        except Exception:
+            pass
     elif getPositions()[coin]['side'] == 'long':
         try:
             return exchange.create_limit_order(
                 coin, 'sell', getPositions()[coin]['contracts'],
                 exchange.fetch_order_book(coin)['asks'][0][0],
                 {'closeOrder': True, 'reduceOnly': True})
-        except Exception as e:
-            print(e)
-
-
-def stoploss():
-    stop = STOP if getPositions()[coin]['percentage'] > TRIGGER else INIT_STOP
-
-    if getPositions()[coin]['percentage'] > trail[coin]:
-        trail[coin] = getPositions()[coin]['percentage']
-    if getPositions()[coin]['percentage'] < (trail[coin]-stop):
-        sell() if getPositions()[coin]['side'] == 'long' else buy()
-    return print('stop: ', stop, 'trail:', trail[coin])
-
-
-def kelts(window=21, period=1):
-    return volatility.keltner_channel_pband(getData(coin, tf)['high'], getData(coin, tf)['low'], getData(coin, tf)['close'], window).iloc[-period]
-
-
+        except Exception:
+            pass
+        
 def bands(window=20, devs=2, period=1):
     return volatility.bollinger_pband(getData(coin, tf)['close'], window, devs).iloc[-period]
 
 
-def kama(ohlc='close', period=1):
-    return momentum.kama(getData(coin, tf)[ohlc]).iloc[-period]
+def kama(ohlc='close', window=8, period=1):
+    return momentum.kama(getData(coin, tf)[ohlc], window).iloc[-period]
 
 
-def rsi(window=2, period=1):
-    return momentum.kama(momentum.rsi(getData(coin, tf)['close'], window)).iloc[-period]
+def rsi(rsi_window=8, kama_window=2, period=1):
+    return momentum.kama(momentum.rsi(getData(coin, tf)['close'], rsi_window), kama_window).iloc[-period]
 
-
-trail = {}
-for coin in COINS:
-    trail[coin] = 0
 
 while True:
     for coin in COINS:
         exchange.cancel_all_orders()
-        print(dataframe(getPositions()))
         for tf in TIMEFRAMES:
 
-            print(dataframe(getPositions()[coin]))
-            print('KARSI:', rsi())
+            buysignal1 = (bands(20, 2) < 0)
+            buysignal2 = (getData(coin, tf)['high'].iloc[-1] >
+                          getData(coin, tf)['high'].iloc[-2] and
+                          getData(coin, tf)['low'].iloc[-1] >
+                          getData(coin, tf)['low'].iloc[-2])
+            buysignal3 = (getData(coin, tf)['close'].iloc[-1] > kama())
 
-            while kelts() > 1 and rsi() > 50 and getData(coin, tf)['close'].iloc[-1] > kama():
+            sellsignal1 = (bands(20, 2) > 1)
+            sellsignal2 = (getData(coin, tf)['high'].iloc[-1] <
+                           getData(coin, tf)['high'].iloc[-2] and
+                           getData(coin, tf)['low'].iloc[-1] <
+                           getData(coin, tf)['low'].iloc[-2])
+            sellsignal3 = (getData(coin, tf)['close'].iloc[-1] < kama())
+
+            while rsi() >= 50 and buysignal3 and buysignal2:
+                print(getPositions())
+                print('ADAPT.RSI:', round(rsi(), 2))
                 buy()
-            while kelts() < 0 and rsi() < 50 and getData(coin, tf)['close'].iloc[-1] < kama():
+                if sellsignal1 or sellsignal2 or sellsignal3:
+                    sell()
+                    break
+                else:
+                    time.sleep(DCA_INTERVAL_DELAY_TIME)
+            
+            while rsi() <= 50 and sellsignal3:
+                print(getPositions())
+                print('ADAPT.RSI:', round(rsi(), 1))
                 sell()
-
-            while getPositions()[coin]['contracts'] != 0:
-                if getPositions()[coin]['side'] == 'long':
-                    sell() if rsi() < 50 else buy()
-                elif getPositions()[coin]['side'] == 'short':
-                    buy() if rsi() > 50 else sell()
+                if buysignal1 or buysignal2 or buysignal3:
+                    buy()
+                    break
+                else:
+                    time.sleep(DCA_INTERVAL_DELAY_TIME)
